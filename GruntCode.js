6a6d9aac3fe341b8ccd2ad66749bf9f59b9395c3
imann24 <imann24@gmail.com>
2014-04-04 09:00:44 -0400

    Trimmed down scripts

--- GruntCode.js
+++ GruntCode.js
@@ -1,17 +1,3 @@
-//<script type="text/javascript" src= "js/phaser.min.js"></script>
-
-
-// var GruntCode = {
-// 	walkRight:function (grunt) {
-// 		enemy.body.velocity = 150;
-// 		enemy.animations.play();
-// 	},
-// 	walkLeft: function (grunt) {
-// 		enemy.body.velocity = -150;
-// 		enemy.animations.play();
-// 	}
-// };
-
 var walkRight = function (enemy) {
 	enemy.body.velocity.x = 150;
 	enemy.animations.play('goRight');
